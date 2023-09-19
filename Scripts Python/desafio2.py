d775c6b15fc400dc1245c6c8d6a6d00bd445e1d9
K.Bruno <kristtoferbruno77@gmail.com>
2023-09-19 10:31:20 -0400

    Add files via upload

---
+++ Scripts Python/desafio2.py
@@ -0,0 +1,8 @@
+#Criar  um script Python que leia o dia,
+#o mês e o ano de nascimento de uma pessoa e mostre uma mensagem
+#com a data formatada.
+
+dia = input('Dia de nascimento?: ')
+mes = input('Mês de nascimento?: ')
+ano = input('Ano de nascimento?: ')
+print('Você nasceu no dia', dia,'de',mes,'de',ano,'. Correto?')
---
+++ Scripts Python/desafio3.py
@@ -0,0 +1,6 @@
+#Crie um script Python que leia  dois números
+#e tente mostrar a soma entre eles.
+
+n1 = float(input('Digite o primeiro número: '))
+n2 = float(input('Digite o segundo número: '))
+print('A soma de',n1,'e',n2,'é igual a:',n1+n2)
